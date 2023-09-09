d1341c66a5e85b67a80cb53a94d7d01b50c5a2f1
Mikko R <yashoo_11@hotmail.fi>
2023-09-09 21:01:01 +0300

    .

--- config.php
+++ config.php
@@ -9,16 +9,6 @@
  * The $site_info array contains information about the website, including the name and description.
  */
 <?php
-// PHP Data Objects(PDO) Sample Code:
-try {
-    $conn = new PDO("sqlsrv:server = tcp:mikkorautavirta.database.windows.net,1433; Database = mikko.rautavirta", "mikko.rautavirta", "{your_password_here}");
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-}
-catch (PDOException $e) {
-    print("Error connecting to SQL Server.");
-    die(print_r($e));
-}
-
 // SQL Server Extension Sample Code:
 $conn -> pg_connect("host=mikkorautavirta.postgres.database.azure.com port=5432 dbname= user=mikko password={your_password}");
 echo $conn;
