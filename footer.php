d0296604cc62fce28b0c2c771e7cc6a0cf8bda35
Mikko R <yashoo_11@hotmail.fi>
2023-09-09 18:08:40 +0300

    some changes and img

--- footer.php
+++ footer.php
@@ -1,16 +1,19 @@
 </div> <!-- Closing tag for the container div started in header.php -->
 
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+
 <script>
-  document.getElementById('langSwitch').addEventListener('click', function() {
+document.getElementById('langSwitch').addEventListener('click', function() {
     if (document.documentElement.lang === 'en') {
-      document.documentElement.lang = 'fi';
-      this.textContent = 'Switch to English';
+        document.documentElement.lang = 'fi';
+        this.textContent = 'Switch to English';
     } else {
-      document.documentElement.lang = 'en';
-      this.textContent = 'Switch to Finnish';
+        document.documentElement.lang = 'en';
+        this.textContent = 'Switch to Finnish';
     }
-  });
+});
 </script>
 </body>
-</html>
--- nav.php
+++ nav.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-lg navbar-light bg-transparent">
   <div class="container-fluid">
     <a class="navbar-brand" href="index.php">
-      <img src="logo.png" alt="Logo" class="sticky-top"> Portfolio
+      <img src="images/logo.jpg" alt="Logo" class="sticky-top"> Portfolio
     </a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
